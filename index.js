c77f813b0e1aaa1c9a66fefce24a8d337a178873
acatl pacheco <acatl.pacheco@gmail.com>
2014-12-17 18:19:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 'use strict';
 
+function getValue(src, def) {
+    return typeof src === 'undefined' ? def : src;
+}
+
+function getValueByKey(key, src, def) {
+    return getValue(src[key], def[key]);
+}
+
+function setKey(key, src, def) {
+    return src[key] = getValue(src[key], def[key]);
+}
+
+function getKeyObject(src, def, factory) {
+    def = getValue(def, {});
+    var value = getValue(src, def);
+    return factory(value);
+}
+
+function setObject(key, src, def, factory) {
+    return src[key] = getKeyObject.call(null, src[key], def[key], factory);
+}
+
+function getCollection(src, defItem, itemFactory) {
+    src = getValue(src, []);
+    return src.map(itemFactory);
+}
+
+function setCollection(key, src, defItem, itemFactory) {
+    return src[key] = getCollection(src[key], defItem, itemFactory);
+}
